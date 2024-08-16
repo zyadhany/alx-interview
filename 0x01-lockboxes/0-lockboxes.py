0c50bd28514be7e49b0c54644438c38f0443f5f9
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 16:19:33 +0300

    locked boxex

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+locked boxes
+"""
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    vis = [False] * n
+
+    def dfs(at):
+        if vis[at]:
+            return 0
+
+        cnt = 1
+        vis[at] = 1
+
+        for neg in boxes[at]:
+            cnt += dfs(neg)
+
+        return cnt
+
+    if dfs(0) == n:
+        return True
+    return False
