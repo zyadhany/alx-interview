2fd1a16e476ab94e8dd39cc55510dd7896e7801e
zyadhany <zeyad.hany2003@gmail.com>
2024-08-31 23:22:02 +0300

    min opp to get h car

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+from queue import Queue
+
+
+def minOperations(n):
+    """
+        min opp to get n
+    """
+
+    que = Queue()
+    que.put((1, 0))
+    vis = {}
+    vis[(1, 0)] = 0
+
+    while not que.empty():
+        m = que.get()
+
+        if m[0] == n:
+            return vis[m]
+
+        ps = (m[0] + m[1], m[1])
+        if ps not in vis and ps[0] <= n:
+            que.put(ps)
+            vis[ps] = vis[m] + 1
+
+        cp = (m[0], m[0])
+        if cp not in vis:
+            que.put(cp)
+            vis[cp] = vis[m] + 1
+
+    return -1
