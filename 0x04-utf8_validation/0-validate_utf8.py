152b3b58238833143fbd5a2dacb412d29ff40486
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 20:19:01 +0300

    task vald

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,6 +4,18 @@ validate
 """
 
 
+def leadOne(num):
+    """
+    get lead 1 of number
+    """
+    tmp = (1 << 7)
+    cnt = 0
+    while tmp & num:
+        tmp >>= 1
+        cnt += 1
+    return cnt
+
+
 def validUTF8(data):
     """
     check utf8 format
@@ -11,17 +23,16 @@ def validUTF8(data):
     left = 0
 
     for ch in data:
-        if left == 0:
-            if ch >> 5 == 0b110:
-                left = 1
-            elif ch >> 4 == 0b1110:
-                left = 2
-            elif ch >> 3 == 0b11110:
-                left = 3
-            elif ch >> 7 != 0:
+        ch &= (1 << 8) - 1
+        if left:
+            lf = (ch >> 6)
+            if lf != 2:
                 return False
         else:
-            if ch >> 6 != 0b10:
+            left = leadOne(ch)
+            if left == 1 or left > 4:
                 return False
-            left -= 1
-    return True
+            if left == 0:
+                continue
+        left -= 1
+    return (left == 0)
