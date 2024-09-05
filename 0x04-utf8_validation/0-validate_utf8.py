9b9a5ddb239c7c67c07166bca1b97e94d9709a8d
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 20:16:12 +0300

    task vald

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -24,15 +24,16 @@ def validUTF8(data):
 
     for ch in data:
         if left == 0:
-            left = leadOne(ch)
-            if left == 1 or left > 4:
+            if ch >> 5 == 0b110:
+                left = 1
+            elif ch >> 4 == 0b1110:
+                left = 2
+            elif ch >> 3 == 0b11110:
+                left = 3
+            elif ch >> 7 != 0:
                 return False
-            if left == 0:
-                continue
         else:
-            lf = (ch >> 6)
-            if not (ch & (1 << 7) and not (ch & (1 << 6))):
+            if ch >> 6 != 0b10:
                 return False
-        left -= 1
-        left = max(left, 0)
+            left -= 1
     return True
