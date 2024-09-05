167534258acaf9435d55a6b43b749de6199e1250
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 20:16:28 +0300

    task vald

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,18 +4,6 @@ validate
 """
 
 
-def leadOne(num):
-    """
-    get lead 1 of number
-    """
-    tmp = (1 << 7)
-    cnt = 0
-    while tmp & num:
-        tmp >>= 1
-        cnt += 1
-    return cnt
-
-
 def validUTF8(data):
     """
     check utf8 format
