47988e8b26822830c06e32696d53a2ddd36b5fb1
zyadhany <zeyad.hany2003@gmail.com>
2024-09-10 14:40:37 +0300

    n quene

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,6 +5,20 @@ n-queens problem
 
 import sys
 
+
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+
 def is_safe(board, row, col):
     """Check if a queen can be placed on board[row][col]"""
     for i in range(col):
@@ -35,18 +49,6 @@ def solve_nqueens(board, col, n, pos):
             pos.pop()
             board[i][col] = 0
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
 
 n = int(sys.argv[1])
 pos = []
