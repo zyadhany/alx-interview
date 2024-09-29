67e60474c6b0de6208d01a87976559edcd38692b
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-29 20:39:31 +0000

    roratat 90

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+
+import numpy as np
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise
+    """
+    n = len(matrix)
+
+    matt = [row[:] for row in matrix]
+
+    for i in range(n):
+        for j in range(n):
+            matrix[i][j] = matt[n - j - 1][i]
