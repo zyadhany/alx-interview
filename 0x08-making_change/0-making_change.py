926a4358d520f53a1d34604f438c27e7fae50b80
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 17:28:02 +0000

    conins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,14 +15,15 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
-
-    dp = np.zeros(total + 1) + 23456789
-    dp[0] = 0
-
-    for i in range(total):
-        for coin in coins:
-            if i + coin <= total:
-                dp[i + coin] = min(dp[i + coin], dp[i] + 1)
-    if dp[total] == 23456789:
-        return -1
-    return int(dp[total])
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
