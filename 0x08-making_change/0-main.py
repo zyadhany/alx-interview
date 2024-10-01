98be954ba916b9c689ff75ff9d40629ffb7f63b2
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 17:34:49 +0000

    conins

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -5,6 +5,4 @@ Main file for testing
 
 makeChange = __import__('0-making_change').makeChange
 
-print(makeChange([1, 2, 25], 37))
-
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+print(makeChange([7, 2], 15))
